23420f967ba93908ba2e58382a20067590aa56a6
Harrison Powers <harrisonpowers@gmail.com>
2026-09-09 10:31:22 -0400

    feat(cli): add device label command

---
+++ cmd/cozysoc-controller/device_label.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/fijimunkii/cozysoc/internal/controller/api"
+	"github.com/fijimunkii/cozysoc/internal/controller/localapi"
+)
+
+func runDeviceLabelCommand(ctx context.Context, args []string, stdout, stderr *os.File) error {
+	fs := flag.NewFlagSet("device-label", flag.ContinueOnError)
+	fs.SetOutput(stderr)
+	stateDir := fs.String("state-dir", "", "controller state directory")
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+	if fs.NArg() != 2 {
+		return fmt.Errorf("device-label requires DEVICE_ID and LABEL")
+	}
+
+	dir, err := resolveStateDir(*stateDir)
+	if err != nil {
+		return err
+	}
+	params := api.DeviceLabelParams{DeviceID: fs.Arg(0), Label: fs.Arg(1)}
+	result, err := localapi.NewClient(dir).CallWithParams(ctx, api.MethodDeviceLabel, params)
+	if err != nil {
+		return err
+	}
+	var pretty api.DeviceLabelResult
+	if err := json.Unmarshal(result, &pretty); err != nil {
+		return fmt.Errorf("decode device label response: %w", err)
+	}
+	enc := json.NewEncoder(stdout)
+	enc.SetIndent("", "  ")
+	return enc.Encode(pretty)
+}
